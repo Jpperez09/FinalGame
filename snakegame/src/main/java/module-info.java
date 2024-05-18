5ac7d6e81ac8d42607f2ed24a0eba164d6dead21
juanpaperez9 <juanpaperez2603@gmail.com>
2024-05-18 15:12:55 -0500

    added on the module the javafx graphics

--- snakegame/src/main/java/module-info.java
+++ snakegame/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.knox.cs.snakegame {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens com.knox.cs.snakegame to javafx.fxml;
     exports com.knox.cs.snakegame;
