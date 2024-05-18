95dfa198e4a621d50768eae83f36cd3a17aea2c0
juanpaperez9 <juanpaperez2603@gmail.com>
2024-05-18 14:48:20 -0500

    modify food class to make the food animated (Bounce animation)

--- snakegame/src/main/java/com/knox/cs/snakegame/Food.java
+++ snakegame/src/main/java/com/knox/cs/snakegame/Food.java
@@ -1,20 +1,29 @@
 package com.knox.cs.snakegame;
 
 import java.util.Random;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 
 public class Food {
     private Point point;
     private static final Random random = new Random();
-    
+    private Timeline bounceTimeline;
+    private int bounceOffset;
+
     public Food() {
         relocate(null);
+        bounceOffset = 0;
+        bounceTimeline = new Timeline(new KeyFrame(Duration.millis(150), e -> bounce()));
+        bounceTimeline.setCycleCount(Timeline.INDEFINITE);
+        bounceTimeline.play();
     }
-    
+
     public void relocate(Snake snake) {
         int x = random.nextInt(30);
         int y = random.nextInt(20);
         point = new Point(x, y);
-        
+
         if (snake != null) {
             while (snake.getBody().contains(point)) {
                 x = random.nextInt(30);
@@ -23,8 +32,12 @@ public class Food {
             }
         }
     }
-    
+
     public Point getPoint() {
-        return point;
+        return new Point(point.getX(), point.getY() + bounceOffset);
+    }
+
+    private void bounce() {
+        bounceOffset = (bounceOffset == 0) ? 1 : 0;
     }
 }
